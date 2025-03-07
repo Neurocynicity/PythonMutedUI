95788fd16d542b6990df92967389ff87bb920c3b
Neurocynicity <105517800+Neurocynicity@users.noreply.github.com>
2025-03-07 20:08:10 +0000

    Made window drags above 30 pixels long no longer clicks

--- ClickableLabel.py
+++ ClickableLabel.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtWidgets import QLabel
+from math import sqrt
 
 
 class ClickableLabel(QLabel):
@@ -8,14 +9,19 @@ class ClickableLabel(QLabel):
     
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            self.pressPos = event.pos()
+            self.pressPos = event.globalPos()
         return super().mousePressEvent(event)
 
     def mouseReleaseEvent(self, event):
-        if (self.pressPos is not None and \
-            event.button() == Qt.LeftButton and \
-            event.pos() in self.rect()):
-            # (event.pos() - self.pressPos)'s magnitude is higher than like 20,20):
+        if self.pressPos is None or \
+             event.button() != Qt.LeftButton:
+            return super().mouseReleaseEvent(event)
+
+        mouseMoveVect = event.globalPos() - self.pressPos
+        mouseMoveDistance = sqrt(mouseMoveVect.x() * mouseMoveVect.x() + mouseMoveVect.y() * mouseMoveVect.y())
+
+        if mouseMoveDistance < 30:
                 self.clicked.emit()
         self.pressPos = None
         return super().mouseReleaseEvent(event)
+        
